d57c51f42fcdbb0e7c8f1f73f992049db0db4292
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2021-07-24 23:31:54 -0300

    Foi feito uma classe para comparar referencias em java

--- 1-Entender a linguagem Java e orientação a objetos/2-Java OO/bytebank/src/CriaConta.java
+++ 1-Entender a linguagem Java e orientação a objetos/2-Java OO/bytebank/src/CriaConta.java
@@ -3,7 +3,14 @@ public class CriaConta {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200;
-        System.out.println(primeiraConta.numero);
+        System.out.println(primeiraConta.saldo);
+        
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 200;
+        System.out.println(segundaConta);
 
     }
 }
