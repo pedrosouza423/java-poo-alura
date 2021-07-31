35dd8e9566e6388bb6767807c340ed4048a3af46
Pedro Souza <62824231+pedrosouza423@users.noreply.github.com>
2021-07-31 00:19:11 -0300

    Foi aplicado a privação de todos os atributos das classes de conta e cliente

--- 1-Entender a linguagem Java e orientação a objetos/2-Java OO/bytebank-encapsulado/src/Cliente.java
+++ 1-Entender a linguagem Java e orientação a objetos/2-Java OO/bytebank-encapsulado/src/Cliente.java
@@ -1,7 +1,32 @@
 public class Cliente {
-        String nome;
-        String cpf;
-        String endereco;
+        private String nome;
+        private String cpf;
+        private String profissao;
 
-        
+        //getters
+        public String getNome() {
+            return nome;
+        }
+
+        public String getCpf() {
+            return cpf;
+        }
+
+        public String getprofissao() {
+            return profissao;
+        }
+
+        //setters
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setCpf(String cpf) {
+            this.cpf = cpf;
+        }
+
+        public void setprofissao(String profissao) {
+            this.profissao = profissao;
+        }
+
 }
--- 1-Entender a linguagem Java e orientação a objetos/2-Java OO/bytebank-encapsulado/src/TestaGetESet.java
+++ 1-Entender a linguagem Java e orientação a objetos/2-Java OO/bytebank-encapsulado/src/TestaGetESet.java
@@ -5,5 +5,17 @@ public class TestaGetESet {
         conta.setNumero(1234);
         System.out.println(conta.getNumero());
 
+        Cliente cliente = new Cliente();
+        conta.setTitular(cliente);
+
+        cliente.setNome("Pedro Henrique");
+        cliente.setCpf("123.456.789-10");
+        cliente.setprofissao("Programador");
+        
+        System.out.println(conta.getTitular().getNome());
+        System.out.println(conta.getTitular().getCpf());
+        System.out.println(conta.getTitular().getprofissao());
+
+
     }
 }
